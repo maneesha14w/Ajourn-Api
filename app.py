1a0d266a8c11abd69ff5825a70eeedf748b691db
Maneesha Walisundera <moonlit741@gmail.com>
2023-04-08 22:25:46 +0530

    new req

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# from keras.preprocessing.text import Tokenizer
-# from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
 
 # import tensorflow as tf
 #import numpy as np
@@ -17,17 +18,17 @@ def sentiment_analysis(text):
     scores = sent.polarity_scores(text)
     return scores
 
-# def input_maker(text):
-#     sent_arr = sentiment_analysis(text)
-#     # Tokenize the input data
-#     tokenizer = Tokenizer(num_words=1000, oov_token='<UNK>')
-#     tokenizer.fit_on_texts(text)
-#     #sequencing
-#     sequences = tokenizer.texts_to_sequences(text)
-#     padded = pad_sequences(sequences, truncating='post', padding='post', maxlen=3483) 
-#     #concating sentiment
-#     inputs = np.concatenate((padded, sent_arr), axis=1)
-#     return inputs
+def input_maker(text):
+    sent_arr = sentiment_analysis(text)
+    # Tokenize the input data
+    tokenizer = Tokenizer(num_words=1000, oov_token='<UNK>')
+    tokenizer.fit_on_texts(text)
+    #sequencing
+    sequences = tokenizer.texts_to_sequences(text)
+    padded = pad_sequences(sequences, truncating='post', padding='post', maxlen=3483) 
+    #concating sentiment
+    inputs = np.concatenate((padded, sent_arr), axis=1)
+    return inputs
 
 
 
@@ -48,12 +49,12 @@ def sentiment_analysis():
     scores = sent.polarity_scores(text)
     return jsonify(scores)
 
-# @app.route('/model', methods=['POST'])
-# def anxiety_detection():
-#     print('Request for anxiety detection received')
-#     text = request.get_json()['text']
-#     input = input_maker(text)
-#     return jsonify(input)
+@app.route('/model', methods=['POST'])
+def anxiety_detection():
+    print('Request for anxiety detection received')
+    text = request.get_json()['text']
+    input = input_maker(text)
+    return jsonify(input)
 
 
 if __name__ == '__main__':
